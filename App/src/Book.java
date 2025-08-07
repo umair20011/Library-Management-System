04bf7a67479cd1fc014c616e8edd9302261c65bd
umair20011 <danishmaniyar2109@gmail.com>
2025-08-07 22:33:17 +0530

    Created Book, User, Library classes

---
+++ App/src/Book.java
@@ -0,0 +1,6 @@
+
+public class Book {
+     int bookId;
+     String bookName;
+     String bookStatus;
+}
---
+++ App/src/User.java
@@ -0,0 +1,6 @@
+
+public class User {
+    int userId;
+    String userName;
+    String userEmail;
+}
